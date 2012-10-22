5bbf3e4a8766d96d4a1dd5d999d7d812bf4df8b0
amanaksu@gmail.com <amanaksu@gmail.com@66d99228-95ba-9187-ebff-a7ff290d9015>
2012-10-22 07:05:06 +0000

    # Crawler 프로그램 테스트중..... - PEScanner.py 추가

---
+++ branches/amanaksu/Crawler/PEScanner.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+# import Public Module
+import traceback
+
+# import Private Module
+from ComFunc import BufferControl
+
+class PEScan():
+    def Check(self, pBuf):
+        try :
+            if BufferControl.Read(pBuf, 0, 2) == "MZ" or BufferControl.Read(pBuf, 0, 2) == "mz" :
+                return "PE"
+            else :
+                return ""
+            
+        except :
+            print traceback.format_exc()
+            
+        
