f1f43b85ebff907752065b6db9e6dde5788d9111
amanaksu@gmail.com <amanaksu@gmail.com@66d99228-95ba-9187-ebff-a7ff290d9015>
2012-10-24 06:29:12 +0000

    # Crawler + VulScan 병합본 테스트 중....

---
+++ branches/amanaksu/AutoScan/OfficeScanner.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+# import Public Module
+import traceback, binascii
+
+
+# import private module
+from Common import BufferControl, FileControl
+
+
+class Office():
+    @classmethod
+    def OfficeScan(cls, File):
+        try :
+            File['logbuf'] += " : Office"
+            
+#            if File["OptSep"] == "*" or File["OptSep"] == "Office" :
+#                if not FileControl.SeperateFile(File, "Office") :
+#                    File['logbuf'] += "\n\t[Failure] Move %s" % File["fname"]            
+#            
+#            return True
+            
+        except :
+            print traceback.format_exc()
+            return False
+        
+        return True
+
+
+
+
